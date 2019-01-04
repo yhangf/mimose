fbc33ecb563a6dd6a62d3ead1ac8e6d81349a124
yhf <yhf5fhy@gmail.com>
2019-01-04 09:12:49 +0800

    add rsign function.

--- mimose/utils/func.py
+++ mimose/utils/func.py
@@ -9,6 +9,7 @@ def sigmoid(X):
     :return: the processed matrix.
     :rtype: np.array and value in [0, 1](dimensions are the same).
     """
+
     return 1 / (1 + np.exp(-X))
 
 def sigmoid_gradient(X):
@@ -19,6 +20,7 @@ def sigmoid_gradient(X):
     :return: gradient matrix.
     :rtype: np.array.
     """
+
     return sigmoid(X) * (1 - sigmoid(X))
 
 def ReLu(X):
@@ -29,6 +31,7 @@ def ReLu(X):
     :return: the processed matrix.
     :rtype: np.array and value in max{0, X}(dimensions are the same).
     """
+
     return np.where(X < 0, 0, X)
 
 def judge(X):
@@ -39,4 +42,15 @@ def judge(X):
     :return: the processed matrix.
     :rtype: np.array and value in {0, 1}(dimensions are the same).
     """
+
     return np.where(X < 0.5, 0, 1)
+
+def rsign(X):
+    """Antisymmetric symbolic functions.
+    :@parma X: raw matrix data.
+    :type X: np.array(M X N).
+    :return: the processed matrix.
+    :rtype: np.array and value in {1, -1}(dimensions are the same).
+    """
+
+    return np.where(X >= 0, 1, -1)
